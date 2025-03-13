af01688bf0169eea34a208412017f0f6959b12d4
Max Gillett <max.gillett@gmail.com>
2025-03-13 15:22:10 -0700

    feat: pre-state root verification in execution of stateless blocks

--- nativerollup/execute/execute.go
+++ nativerollup/execute/execute.go
@@ -12,20 +12,24 @@ import (
 	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/beacon"
+	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/stateless"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/triedb"
 	"github.com/prysmaticlabs/go-ssz"
 )
 
 type ExecutionBlock struct {
 	PreStateRoot  common.Hash
 	PostStateRoot common.Hash
-	GasUsed       uint64
 	Witness       *stateless.Witness
 	Header        types.Header
 	Transactions  types.Transactions
@@ -44,7 +48,7 @@ type CalldataInput struct {
 	CompressedPayload []byte
 }
 
-func DecodeExecutionInput(input []byte) (*ExecutionTrace, error) {
+func decodeExecutionInput(input []byte) (*ExecutionTrace, error) {
 	var raw CalldataInput
 	if err := rlp.DecodeBytes(input, &raw); err != nil {
 		return nil, fmt.Errorf("invalid input format: %w", err)
@@ -75,7 +79,7 @@ func DecodeExecutionInput(input []byte) (*ExecutionTrace, error) {
 	for i := range raw.Blocks {
 		trace.Blocks[i].PreStateRoot = raw.Blocks[i].PreStateRoot
 		trace.Blocks[i].PostStateRoot = raw.Blocks[i].PostStateRoot
-		trace.Blocks[i].GasUsed = raw.Blocks[i].GasUsed.Uint64()
+		trace.Blocks[i].Header.GasUsed = raw.Blocks[i].GasUsed.Uint64()
 	}
 
 	return &trace, nil
@@ -83,7 +87,7 @@ func DecodeExecutionInput(input []byte) (*ExecutionTrace, error) {
 
 // ExecutePrecompile handles the full execution flow from input decoding to state validation
 func ExecutePrecompile(input []byte, chainConfig *params.ChainConfig, vmConfig *vm.Config) ([]byte, error) {
-	trace, err := DecodeExecutionInput(input)
+	trace, err := decodeExecutionInput(input)
 	if err != nil {
 		return []byte{0}, err
 	}
@@ -98,7 +102,7 @@ func ExecutePrecompile(input []byte, chainConfig *params.ChainConfig, vmConfig *
 		}
 	}
 
-	// Then perform concurrent execution validation
+	// Then perform concurrent execution verification
 	var (
 		wg          sync.WaitGroup
 		errCh       = make(chan error, len(trace.Blocks))
@@ -117,12 +121,6 @@ func ExecutePrecompile(input []byte, chainConfig *params.ChainConfig, vmConfig *
 			default:
 			}
 
-			if block.Header.GasUsed != block.GasUsed {
-				errCh <- fmt.Errorf("block %x gas used mismatch", block.Header.Hash())
-				cancel()
-				return
-			}
-
 			blockInst := types.NewBlock(
 				&block.Header,
 				&types.Body{Transactions: block.Transactions},
@@ -130,10 +128,11 @@ func ExecutePrecompile(input []byte, chainConfig *params.ChainConfig, vmConfig *
 				trie.NewStackTrie(nil),
 			)
 
-			stateRoot, _, err := core.ExecuteStateless(
+			stateRoot, _, err := executeStateless(
 				chainConfig,
 				*vmConfig,
 				blockInst,
+				block.PreStateRoot,
 				block.Witness,
 			)
 			if err != nil {
@@ -167,4 +166,46 @@ func ExecutePrecompile(input []byte, chainConfig *params.ChainConfig, vmConfig *
 	}
 
 	return []byte{1}, nil
+}
+
+// Modified from core.ExecuteStateless to also verify pre-state root
+func executeStateless(config *params.ChainConfig, vmconfig vm.Config, block *types.Block, preStateRoot common.Hash, witness *stateless.Witness) (common.Hash, common.Hash, error) {
+	// Sanity check if the supplied block accidentally contains a set root or
+	// receipt hash. If so, be very loud, but still continue.
+	if block.Root() != (common.Hash{}) {
+		log.Error("stateless runner received state root it's expected to calculate (faulty consensus client)", "block", block.Number())
+	}
+	if block.ReceiptHash() != (common.Hash{}) {
+		log.Error("stateless runner received receipt root it's expected to calculate (faulty consensus client)", "block", block.Number())
+	}
+
+	// Create and populate the state database to serve as the stateless backend
+	memdb := witness.MakeHashDB()
+	db, err := state.New(witness.Root(), state.NewDatabase(triedb.NewDatabase(memdb, triedb.HashDefaults), nil))
+	if err != nil {
+		return common.Hash{}, common.Hash{}, err
+	}
+
+	// Verify that the preStateRoot matches the instantiated state database
+	if preStateRoot != db.IntermediateRoot(false) {
+		return common.Hash{}, common.Hash{}, fmt.Errorf("Pre-state root does not match instantiated state db root")
+	}
+
+	// Create a blockchain that is idle, but can be used to access headers through
+	chain, _ := core.NewHeaderChain(memdb, config, beacon.New(ethash.NewFaker()), func() bool { return false })
+	processor := core.NewStateProcessor(config, chain)
+	validator := core.NewBlockValidator(config, nil) // No chain, we only validate the state, not the block
+
+	// Run the stateless blocks processing and self-validate certain fields
+	res, err := processor.Process(block, db, vmconfig)
+	if err != nil {
+		return common.Hash{}, common.Hash{}, err
+	}
+	if err = validator.ValidateState(block, db, res, true); err != nil {
+		return common.Hash{}, common.Hash{}, err
+	}
+	// Almost everything validated, but receipt and state root needs to be returned
+	receiptRoot := types.DeriveSha(res.Receipts, trie.NewStackTrie(nil))
+	stateRoot := db.IntermediateRoot(config.IsEIP158(block.Number()))
+	return stateRoot, receiptRoot, nil
 }
